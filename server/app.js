8300a903c1560bb88ebce61192dd3abae44a2cc9
Ismail Opatola <opatolamails@gmail.com>
2019-08-02 05:09:59 +0100

    grabbing and using our schema

--- server/app.js
+++ server/app.js
@@ -1,6 +1,15 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql')
-const app = express()
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema
+  })
+);
+
+const app = express();
 app.listen(4000, () => {
-   console.log('now listening for request on port 4000');
+  console.log("now listening for request on port 4000");
 });
