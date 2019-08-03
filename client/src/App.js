aa999e1739e96622804bf752b017fd3ffc8699ab
Ismail Opatola <opatolamails@gmail.com>
2019-08-03 08:03:57 +0100

    create BookList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import BookList from './components/BookList'
 function App() {
   return (
     <div className="App">
       <h1>Reading List</h1>
+      <BookList />
     </div>
   );
 }
